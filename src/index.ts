40480beb83a049a3ffa233549ee4a36b3dc44f97
deft <deftmail@163.com>
2020-12-02 16:48:34 +0800

    获取响应数据

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
+import { AxiosPromise } from './types/index'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { processHeaders } from './helpers/helpers'
 
-function axios(config: AxiosRequestConfig) {
+function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  xhr(config)
+  return xhr(config)
 }
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
